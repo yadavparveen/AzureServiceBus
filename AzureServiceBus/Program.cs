f3bc50b0e886f28cf7b293bab89d5b04b9047f1b
parveen.kumar <parveen.kumar@technossus.com>
2021-06-04 15:48:47 +0530

    removed keys

--- AzureServiceBus/Program.cs
+++ AzureServiceBus/Program.cs
@@ -10,8 +10,8 @@ namespace AzureServiceBus
         {
             //how to get connection string? here is the docuement
             //https://social.msdn.microsoft.com/Forums/azure/en-US/c8edd9b5-76ea-4d93-8025-2e9d90b5ddf4/where-to-find-the-connectionstring-in-the-new-service-bus-portal?forum=servbus
-            var connectionString = "Endpoint=sb://sbs-usw1-ood-slot1-dev.servicebus.windows.net/;SharedAccessKeyName=admin;SharedAccessKey=PCelvETZinCvNGP4suHM4bW3MNtCH4LpijX9ol7y2wU=;EntityPath=pdm-test-queue";
-            var queueName = "pdm-test-queue";
+            var connectionString = "{your connection strin here}";
+            var queueName = "{ your queue name here}";
 
             // send a message to the queue
             await QueueSender.SendMessageAsync(connectionString, queueName);
